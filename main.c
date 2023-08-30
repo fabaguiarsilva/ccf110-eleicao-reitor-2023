2d88de8bd5ec2d4aa2ba246ebb0e56014357a516
Fabricio <fabaguiarsilva@gmail.com>
2023-08-30 08:41:34 -0300

    Leitura dos votos de 5 eleitores e exibição dos votos para cada candidato.

--- main.c
+++ main.c
@@ -17,6 +17,7 @@ int main(){
     n_votos_candidato2 = 0;
     n_votos_candidato3 = 0;
 
+    //Leitura dos votos
     printf("Voto do eleitor 1: ");
     scanf("%d",&voto_eleitor1);
 
@@ -40,6 +41,62 @@ int main(){
     printf("Eleitor 4: %d\n",voto_eleitor4);
     printf("Eleitor 5: %d\n",voto_eleitor5);
 
+    //Contagem dos votos
+    if(voto_eleitor1 == 1) {
+        n_votos_candidato1++;
+    }else if(voto_eleitor1 == 2){
+        n_votos_candidato2++;
+    }else if(voto_eleitor1 == 3){
+        n_votos_candidato3++;
+    }else{
+        printf("Eleitor 1 votou nulo");
+    }
+    
+    if(voto_eleitor2 == 1) {
+        n_votos_candidato1++;
+    }else if(voto_eleitor2 == 2){
+        n_votos_candidato2++;
+    }else if(voto_eleitor2 == 3){
+        n_votos_candidato3++;
+    }else{
+        printf("Eleitor 2 votou nulo");
+    }
+
+    if(voto_eleitor3 == 1) {
+        n_votos_candidato1++;
+    }else if(voto_eleitor3 == 2){
+        n_votos_candidato2++;
+    }else if(voto_eleitor3 == 3){
+        n_votos_candidato3++;
+    }else{
+        printf("Eleitor 3 votou nulo");
+    }   
+
+    if(voto_eleitor4 == 1) {
+        n_votos_candidato1++;
+    }else if(voto_eleitor4 == 2){
+        n_votos_candidato2++;
+    }else if(voto_eleitor4 == 3){
+        n_votos_candidato3++;
+    }else{
+        printf("Eleitor 4 votou nulo");
+    }
+
+    if(voto_eleitor5 == 1) {
+        n_votos_candidato1++;
+    }else if(voto_eleitor5 == 2){
+        n_votos_candidato2++;
+    }else if(voto_eleitor5 == 3){
+        n_votos_candidato3++;
+    }else{
+        printf("Eleitor 5 votou nulo");
+    }
+
+    //Apuração
+    printf("Votos por candidato:\n");
+    printf("Candidato 1: %d\n",n_votos_candidato1);
+    printf("Candidato 2: %d\n",n_votos_candidato2);
+    printf("Candidato 3: %d\n",n_votos_candidato3);
 
     return 0;
 }
