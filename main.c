3722387afe2250a7549691166877bdea09d233ac
Fabricio <fabaguiarsilva@gmail.com>
2023-09-06 08:51:06 -0300

    Adicionada apuração, sem tratar empate

--- main.c
+++ main.c
@@ -35,13 +35,24 @@ int main(){
         }
     }
 
-    //Apuração
+    //Impressão dos
     printf("Votos por candidato:\n");
     printf("Candidato 1: %d\n",votos_candidatos[0]);
     printf("Candidato 2: %d\n",votos_candidatos[1]);
     printf("Candidato 3: %d\n",votos_candidatos[2]);
     printf("Nulos: %d\n",n_votos_nulos);
     
+    int indice_vencedor = 0;
+    for(int i=1; i<n_candidatos; i++){
+        if(votos_candidatos[i] 
+            > votos_candidatos[indice_vencedor]){
+                indice_vencedor = i;
+        }
+    }
+
+    printf("O vencedor é o candidato %d com %d votos\n",
+            indice_vencedor+1, votos_candidatos[indice_vencedor]);
+
 
     return 0;
 }
